626d64b12b6f15147504e31b0849d6e3c00e18ee
ValeryiaMIRON <76487684+ValeryiaMIRON@users.noreply.github.com>
2021-03-10 19:41:50 +0300

    feat: add code

--- src/index.js
+++ src/index.js
@@ -1,12 +1,43 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min (array = "0") {
+    let minValue = 0;
+    if (array == 0 ) {
+        return 0;
+    } else {
+    for (let i = 0;i < array.length;i++) {
+      if (array[i] < minValue) {
+        minValue = array[i];
+        } 
+      }
+    }
+      return minValue;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max (array = "0") {
+    let maxValue = 0;
+    if (array == 0) {
+      return 0;
+      } else {
+    for (let i = 0;i < array.length;i++) {
+      if (array[i] > maxValue) {
+        maxValue = array[i];
+        } 
+      }
+      }
+      return maxValue;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg (array = "0") {
+    let value = 0;
+    let avgValue;
+    if (array == 0) {
+        return 0;
+    } else {
+    for (let i = 0;i < array.length;i++) {
+      value += array[i];
+      avgValue = value / array.length;
+      }
+    }
+    
+   return avgValue;
 }
